57cc729eed822de651337512e08082d79dfcaaa6
Brittany Javalera <brittjavalera@gmail.com>
2019-10-23 20:01:54 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(store: 'Wholefoods', coupon_code: 'apples')
+Coupon.create(store: 'Target', coupon_code: 'DoveSoap')
